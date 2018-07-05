b705c4c57b1d0314a57a8b68b22080c5e963ae14
Ilmio <facchin.carlos@yahoo.fr>
2018-07-05 16:55:55 +0200

    ajout des components manquants et modifications du code

--- front/src/containers/login.js
+++ front/src/containers/login.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react"
-import styled from "styled-components"
 
-const Header = styled.div`
-  background-color: ${props => props.theme.color.black};
-  height: 150px;
-  padding: 20px;
-  color: white;
-`
+// import styled from "styled-components"
+import './App.css'
+
+import Header from "../components/Header";
+import Formulaire from "../components/Formulaire";
+import Footer from "../components/Footer";
+
+// const Header = styled.div`
+//   background-color: ${props => props.theme.color.black};
+//   height: 150px;
+//   padding: 20px;
+//   color: white;
+// `
 
 class Login extends Component {
   state = {
@@ -17,10 +23,15 @@ class Login extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <Header />
-        <p>Ceci est la page de login</p>
-      </React.Fragment>
+      <div className="App">
+        <div className="Container">
+          <React.Fragment>
+            <Header />
+            <Formulaire />
+            <Footer />
+          </React.Fragment>
+        </div>
+      </div>
     )
   }
 }
