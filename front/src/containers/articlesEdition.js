209440af1cbfb9dec2885440f090d582415a332d
Ilmio <facchin.carlos@yahoo.fr>
2018-07-05 16:49:30 +0200

    ajout App.css dans l'import ?

--- front/src/containers/articlesEdition.js
+++ front/src/containers/articlesEdition.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import TopMenu from "../components/topMenu";
 import List from "../components/listArticle";
 
+import './App.css'
+
 class ArticleEdition extends Component {
   render() {
     return (
